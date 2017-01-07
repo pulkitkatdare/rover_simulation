b03068237245cabb66cca33f4aea460cdfb67482
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-01-08 00:50:15 +0530

    Added Husky simulator plugin

--- autonomous_ws/src/husky_simulator/include/husky_simulator/husky_sim_plugin.hh
+++ autonomous_ws/src/husky_simulator/include/husky_simulator/husky_sim_plugin.hh
@@ -4,13 +4,38 @@
 #include <gazebo/common/common.hh>
 #include <gazebo/common/Plugin.hh>
 
+#include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+
 namespace gazebo
 {
-  class MoveHusky : public ModelPlugin
-  {
-    public: MoveHusky();
-    public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
-  };
+	class MoveHusky : public ModelPlugin
+	{
+		public: MoveHusky();
+		public: void Load(physics::ModelPtr, sdf::ElementPtr);
+		public: void LoadThread();
+		public: void HuskyStateCallback(const std_msgs::Float64MultiArray::ConstPtr&);
+		public: void OnUpdate(const common::UpdateInfo&);
 
-  GZ_REGISTER_MODEL_PLUGIN(MoveHusky)
+		// Global pointer declarations
+		private: physics::ModelPtr husky;
+		private: physics::WorldPtr world;
+		private: sdf::ElementPtr sdf;
+		private: event::ConnectionPtr _updateConnection;
+		private: boost::thread _spinnerThread;
+
+		private: physics::JointPtr front_left_joint;
+		private: physics::JointPtr front_right_joint;
+		private: physics::JointPtr back_left_joint;
+		private: physics::JointPtr back_right_joint;
+		private: float front_left_wrench;
+		private: float front_right_wrench;
+		private: float back_left_wrench;
+		private: float back_right_wrench;
+
+		private: ros::Subscriber subscriber;
+
+	};
+
+	GZ_REGISTER_MODEL_PLUGIN(MoveHusky);
 }
--- autonomous_ws/src/husky_simulator/src/husky_sim_plugin.cc
+++ autonomous_ws/src/husky_simulator/src/husky_sim_plugin.cc
@@ -3,10 +3,76 @@
 namespace gazebo {
 
 	MoveHusky::MoveHusky() : ModelPlugin() {
-		printf("Hello World!\n");
+		ROS_INFO("Husky with Hokuyo added to world.\n");
 	}
 
 	void MoveHusky::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
 
+		if (!ros::isInitialized()) {
+			
+			ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin.\n");
+			return;
+	
+	    	this->husky = _model;
+			this->sdf   = _sdf;
+			this->world = husky->GetWorld();
+
+			this->front_left_joint  = husky->GetJoint("front_left_joint");
+			this->front_right_joint = husky->GetJoint("front_right_joint");
+			this->back_left_joint   = husky->GetJoint("back_left_joint");
+			this->back_right_joint  = husky->GetJoint("back_right_joint");
+
+			this->front_left_wrench  = 0;
+			this->front_right_wrench = 0;
+			this->back_left_wrench   = 0;
+			this->back_right_wrench	 = 0;
+
+			this->_spinnerThread = boost::thread(boost::bind(&MoveHusky::LoadThread, this));
+
+		}
+
 	}
+
+	void MoveHusky::LoadThread() {
+
+		if(!this->husky){
+			ROS_FATAL_STREAM("Husky plugin requires a husky as its parent.\n");
+			return;
+		}
+
+		ros::NodeHandle _nh("husky_sim_plugin");
+		subscriber = _nh.subscribe("husky_directives", 1000, &MoveHusky::HuskyStateCallback, this);
+		this->_updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&MoveHusky::OnUpdate, this, _1));
+
+		ROS_INFO("Husky plugin for ROS successfully loaded.\n");
+
+		// Check for new messages
+		//      ros::spin();
+		while(ros::ok()){
+			ros::spinOnce();
+		}
+	}
+
+	void MoveHusky::HuskyStateCallback(const std_msgs::Float64MultiArray::ConstPtr& rx_msg) {
+		front_left_wrench  = rx_msg->data[0];
+		front_right_wrench = rx_msg->data[1];
+		back_left_wrench   = rx_msg->data[2];
+		back_right_wrench  = rx_msg->data[3];
+	}
+
+	void MoveHusky::OnUpdate(const common::UpdateInfo &) {
+		if ( subscriber.getNumPublishers() != 0 ) {
+		    this->front_left_joint->SetForce(0,front_left_wrench);
+		    this->front_right_joint->SetForce(0,front_right_wrench);
+		    this->back_left_joint->SetForce(0,back_left_wrench);
+		    this->back_right_joint->SetForce(0,back_right_wrench);
+		  }
+		  else {
+		    this->front_left_joint->SetForce(0,0);
+		    this->front_right_joint->SetForce(0,0);
+		    this->back_left_joint->SetForce(0,0);
+		    this->back_right_joint->SetForce(0,0);
+		}
+	}
+
 }
