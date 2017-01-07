a7e6b6808e04e45421b199c123819a8556b092e2
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-01-07 23:02:41 +0530

    Cleanp

--- autonomous_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws/src
-# Build directory: /home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
