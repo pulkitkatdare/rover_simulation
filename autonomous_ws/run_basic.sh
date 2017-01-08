810d33fb13c252c7a895bf481c9aece1901d4823
Alankar Kotwal <alankarkotwal13@gmail.com>
2017-01-08 20:04:24 +0530

    Added launchfile for husky_sim_test

--- autonomous_ws/run_basic.sh
+++ autonomous_ws/run_basic.sh
@@ -1,3 +1,10 @@
-export GAZEBO_MODEL_PATH=./gazebo/models
+#!/bin/bash
 
-gazebo ./gazebo/worlds/saved.world
+source /opt/ros/indigo/setup.bash
+source devel/setup.bash
+
+#export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:./gazebo/models
+#export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:./gazebo:./gazebo/worlds
+
+#roslaunch husky_simulator test.launch
+gazebo gazebo/worlds/saved.world
---
+++ autonomous_ws/src/husky_simulator/src/husky_sim_test.cpp
@@ -0,0 +1,5 @@
+#include <ros/ros.h>
+
+int main() {
+
+}
