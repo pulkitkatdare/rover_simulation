604cc759a4b071ecab3014018581adba308310ea
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-02-13 23:49:06 +0530

    Rover revamp

--- autonomous_ws_old/build/catkin_generated/setup_cached.sh
+++ autonomous_ws_old/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export PWD="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws/build"
+export CMAKE_PREFIX_PATH="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/devel/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/opt/ros/indigo/bin:/home/dhruv-shah/.rbenv/shims:/home/dhruv-shah/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PKG_CONFIG_PATH="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/devel/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws/src:/home/dhruv-shah/ros_ws/rover_simulation/autonomous_ws_old/src:$ROS_PACKAGE_PATH"
