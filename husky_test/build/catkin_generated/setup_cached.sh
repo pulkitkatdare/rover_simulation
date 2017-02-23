540ea246088cc615033c29ae8953fb80086112b8
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-02-24 01:13:40 +0530

    CDR Preps

--- husky_test/build/catkin_generated/setup_cached.sh
+++ husky_test/build/catkin_generated/setup_cached.sh
@@ -5,17 +5,14 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export HUSKY_MAG_CONFIG="/opt/ros/indigo/share/husky_bringup/config/mag_config_default.yaml"
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/include:$CPATH"
+export CMAKE_PREFIX_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel:/opt/ros/indigo"
+export CPATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/include:/opt/ros/indigo/include"
 export HUSKY_DESCRIPTION="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/src/husky_description/urdf/description.xacro"
 export HUSKY_GAZEBO_DESCRIPTION="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/src/husky_gazebo/urdf/description.gazebo.xacro"
-export LD_LIBRARY_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/x86_64-linux-gnu"
-export PATH="/opt/ros/indigo/bin:/home/dhruv-shah/.rbenv/shims:/home/dhruv-shah/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
-export PKG_CONFIG_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig"
+export LD_LIBRARY_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/dhruv-shah/ros_ws/rover_simulation_ref/husky_test/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/dhruv-shah/ros_ws/rover_simulation_ref/husky_test/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/build"
-export PYTHONPATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export PYTHONPATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/dhruv-shah/ros_ws/rover_simulation/husky_test/src:$ROS_PACKAGE_PATH"
